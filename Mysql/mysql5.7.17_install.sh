fa800181f730f90a857ce8e018ecb3f750d68e63
jiangnan <zhutongcloud@163.com>
2019-08-20 18:31:46 +0800

    Update mysql5.7.17_install.sh

--- Mysql/mysql5.7.17_install.sh
+++ Mysql/mysql5.7.17_install.sh
@@ -13,7 +13,7 @@ fi
 yum install -y wget
 cd /usr/local/
 if [  -d mysql ];then
-   	mv mysql mysql`date +%Y-%m-%d_%H-%M-%S`.bak
+   	mv mysql mysql_`date +%Y-%m-%d`.bak
 fi
 if [ ! -f mysql-5.7.17-linux-glibc2.5-x86_64.tar.gz ];then
 	wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.17-linux-glibc2.5-x86_64.tar.gz
